811f998994117a4a42817e4cbf12068b592e3b39
Jiaxi Hu <jiaxihu@gmail.com>
2013-10-29 22:03:43 -0400

    back to working status

--- common/game.cpp
+++ common/game.cpp
@@ -7,8 +7,8 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
-#include <iostream>
-#include <fstream>
+//#include <iostream>
+//#include <fstream>
 #include <cmath>
 #include <string.h>
 #include <cstdlib>
@@ -142,56 +142,56 @@ static void draw(void) {
 	}
 }
 
-unsigned long getFileLength(ifstream& file) {
-	if (!file.good())
-		return 0;
-
-	unsigned long pos = file.tellg();
-	file.seekg(0, ios::end);
-	unsigned long len = file.tellg();
-	file.seekg(ios::beg);
-
-	return len;
-}
-
-int loadshader(char* filename, GLchar** ShaderSource, unsigned long* len) {
-	ifstream file;
-	file.open(filename, ios::in); // opens as ASCII!
-	if (!file)
-		return -1;
-
-	len = getFileLength(file);
-
-	if (len == 0)
-		return -2;   // Error: Empty File
-
-	*ShaderSource = (GLubyte*) new char[len + 1];
-	if (*ShaderSource == 0)
-		return -3;   // can't reserve memory
-
-	// len isn't always strlen cause some characters are stripped in ascii read...
-	// it is important to 0-terminate the real length later, len is just max possible value...
-	*ShaderSource[len] = 0;
-
-	unsigned int i = 0;
-	while (file.good()) {
-		*ShaderSource[i] = file.get();       // get character from file.
-		if (!file.eof())
-			i++;
-	}
-
-	*ShaderSource[i] = 0;  // 0-terminate it at the correct position
-
-	file.close();
-
-	return 0; // No Error
-}
-
-int unloadshader(GLubyte** ShaderSource) {
-	if (*ShaderSource != 0)
-		delete[] *ShaderSource;
-	*ShaderSource = 0;
-}
+//unsigned long getFileLength(ifstream& file) {
+//	if (!file.good())
+//		return 0;
+//
+//	unsigned long pos = file.tellg();
+//	file.seekg(0, ios::end);
+//	unsigned long len = file.tellg();
+//	file.seekg(ios::beg);
+//
+//	return len;
+//}
+//
+//int loadshader(char* filename, GLchar** ShaderSource, unsigned long* len) {
+//	ifstream file;
+//	file.open(filename, ios::in); // opens as ASCII!
+//	if (!file)
+//		return -1;
+//
+//	len = getFileLength(file);
+//
+//	if (len == 0)
+//		return -2;   // Error: Empty File
+//
+//	*ShaderSource = (GLubyte*) new char[len + 1];
+//	if (*ShaderSource == 0)
+//		return -3;   // can't reserve memory
+//
+//	// len isn't always strlen cause some characters are stripped in ascii read...
+//	// it is important to 0-terminate the real length later, len is just max possible value...
+//	*ShaderSource[len] = 0;
+//
+//	unsigned int i = 0;
+//	while (file.good()) {
+//		*ShaderSource[i] = file.get();       // get character from file.
+//		if (!file.eof())
+//			i++;
+//	}
+//
+//	*ShaderSource[i] = 0;  // 0-terminate it at the correct position
+//
+//	file.close();
+//
+//	return 0; // No Error
+//}
+//
+//int unloadshader(GLubyte** ShaderSource) {
+//	if (*ShaderSource != 0)
+//		delete[] *ShaderSource;
+//	*ShaderSource = 0;
+//}
 
 static void create_shaders(void) {
 	static const char *fragShaderText = "precision mediump float;\n"
@@ -212,57 +212,57 @@ static void create_shaders(void) {
 	GLuint fragShader, vertShader, program;
 	GLint stat;
 
-//	fragShader = glCreateShader(GL_FRAGMENT_SHADER);
-//	glShaderSource(fragShader, 1, (const char **) &fragShaderText, NULL);
-//	glCompileShader(fragShader);
-//	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &stat);
-//	if (!stat) {
-////      printf("Error: fragment shader did not compile!\n");
-//		exit(1);
-//	}
-//
-//	vertShader = glCreateShader(GL_VERTEX_SHADER);
-//	glShaderSource(vertShader, 1, (const char **) &vertShaderText, NULL);
-//	glCompileShader(vertShader);
-//	glGetShaderiv(vertShader, GL_COMPILE_STATUS, &stat);
-//	if (!stat) {
-////      printf("Error: vertex shader did not compile!\n");
-//		exit(1);
-//	}
-
-	unsigned long file_len;
-	GLchar ** shadersource;
-
-
-	const char fragname[] = "simplefrag.frag";
-	file_len = getFileLength(fragname);
-	loadshader(fragname, shadersource, file_len);
-
 	fragShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragShader, 1, shadersource, NULL);
+	glShaderSource(fragShader, 1, (const char **) &fragShaderText, NULL);
 	glCompileShader(fragShader);
 	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &stat);
 	if (!stat) {
-		//      printf("Error: fragment shader did not compile!\n");
+//      printf("Error: fragment shader did not compile!\n");
 		exit(1);
 	}
 
-	unloadshader((GLubyte**) shadersource);
-
-	const char fragname[] = "simplevert.vert";
-	file_len = getFileLength(fragname);
-	loadshader(fragname, shadersource, file_len);
-
 	vertShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertShader, 1, shadersource, NULL);
+	glShaderSource(vertShader, 1, (const char **) &vertShaderText, NULL);
 	glCompileShader(vertShader);
 	glGetShaderiv(vertShader, GL_COMPILE_STATUS, &stat);
 	if (!stat) {
-		//      printf("Error: vertex shader did not compile!\n");
+//      printf("Error: vertex shader did not compile!\n");
 		exit(1);
 	}
 
-	unloadshader((GLubyte**) shadersource);
+//	unsigned long file_len;
+//	GLchar ** shadersource;
+//
+//
+//	const char fragname[] = "simplefrag.frag";
+//	file_len = getFileLength(fragname);
+//	loadshader(fragname, shadersource, file_len);
+//
+//	fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+//	glShaderSource(fragShader, 1, shadersource, NULL);
+//	glCompileShader(fragShader);
+//	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &stat);
+//	if (!stat) {
+//		//      printf("Error: fragment shader did not compile!\n");
+//		exit(1);
+//	}
+//
+//	unloadshader((GLubyte**) shadersource);
+//
+//	const char fragname[] = "simplevert.vert";
+//	file_len = getFileLength(fragname);
+//	loadshader(fragname, shadersource, file_len);
+//
+//	vertShader = glCreateShader(GL_VERTEX_SHADER);
+//	glShaderSource(vertShader, 1, shadersource, NULL);
+//	glCompileShader(vertShader);
+//	glGetShaderiv(vertShader, GL_COMPILE_STATUS, &stat);
+//	if (!stat) {
+//		//      printf("Error: vertex shader did not compile!\n");
+//		exit(1);
+//	}
+//
+//	unloadshader((GLubyte**) shadersource);
 
 	program = glCreateProgram();
 	glAttachShader(program, fragShader);
