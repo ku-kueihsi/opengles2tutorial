581079116836ed06e4990ad1a525b291e4975fcb
jesse.laptop <jiaxihu@gmail.com>
2013-12-17 21:35:45 -0500

    testing tegra2

--- src/platform/android/jni/Android.mk
+++ src/platform/android/jni/Android.mk
@@ -1,5 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
+##eigen3
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := eigen3
+#LOCAL_EXPORT_C_INCLUDES := $(ANDROID_TOOLCHAIN)/include/eigen3
+#include $(PREBUILT_STATIC_LIBRARY)
+
 #bullet
 include $(CLEAR_VARS)
 LOCAL_MODULE := bullet
@@ -32,19 +38,13 @@ LOCAL_EXPORT_C_INCLUDES := $(ANDROID_TOOLCHAIN)/include/
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-
-#LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../3rdparty/eigen3/
-#LOCAL_C_INCLUDES := /home/jesse/local/android_toolchain/include/eigen3/
-LOCAL_C_INCLUDES := $(ANDROID_TOOLCHAIN)/include/
-LOCAL_C_INCLUDES += $(ANDROID_TOOLCHAIN)/include/eigen3/
-LOCAL_C_INCLUDES += $(ANDROID_SYSROOT)/include/
-
 LOCAL_MODULE    := game
-LOCAL_CFLAGS    := -Wall -Wextra
-LOCAL_CPPFLAGS  += -std=c++11
+LOCAL_C_INCLUDES += $(ANDROID_TOOLCHAIN)/include/eigen3/
+LOCAL_CFLAGS    := -Wall -Wextra -O2
+LOCAL_CFLAGS    += -Wno-psabi -march=armv7-a -mfloat-abi=softfp -mfpu=vfp -ffunction-sections -funwind-tables -fstack-protector -fno-short-enums -DANDROID -Wa,--noexecstack -std=gnu++0x -mtune=cortex-a9 -march=armv7-a -mhard-float -mfloat-abi=softfp -mfpu=vfpv3-d16 -g -g -gdwarf-2 -marm -O0 -fno-omit-frame-pointer -Wall -Wno-psabi -W -D_REENTRANT -fPIE -DQT_QML_DEBUG -DQT_DECLARATIVE_DEBUG -DQT_OPENGL_LIB -DQT_WIDGETS_LIB -DQT_GUI_LIB -DQT_CORE_LIB
+LOCAL_CPPFLAGS  += -std=c++11 -O2
+LOCAL_CPPFLAGS  += -Wno-psabi -march=armv7-a -mfloat-abi=softfp -mfpu=vfp -ffunction-sections -funwind-tables -fstack-protector -fno-short-enums -DANDROID -Wa,--noexecstack -std=gnu++0x -mtune=cortex-a9 -march=armv7-a -mhard-float -mfloat-abi=softfp -mfpu=vfpv3-d16 -g -g -gdwarf-2 -marm -O0 -fno-omit-frame-pointer -Wall -Wno-psabi -W -D_REENTRANT -fPIE -DQT_QML_DEBUG -DQT_DECLARATIVE_DEBUG -DQT_OPENGL_LIB -DQT_WIDGETS_LIB -DQT_GUI_LIB -DQT_CORE_LIB 
 LOCAL_SRC_FILES := ../../common/game.cpp jni.cpp assets_file_io.cpp
 LOCAL_STATIC_LIBRARIES := assimp bullet libpng libz
 LOCAL_LDLIBS 	:= -lstdc++ -llog -lGLESv2 -lEGL -landroid
-
-
 include $(BUILD_SHARED_LIBRARY)
