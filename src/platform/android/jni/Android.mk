30ece662724ce348cbf0fdf39651acefdd87af07
jesse.laptop <jiaxihu@gmail.com>
2014-01-28 20:20:50 -0500

    exclude bullet for now

--- src/platform/android/jni/Android.mk
+++ src/platform/android/jni/Android.mk
@@ -6,15 +6,15 @@ LOCAL_PATH := $(call my-dir)
 #LOCAL_EXPORT_C_INCLUDES := $(ANDROID_TOOLCHAIN)/include/eigen3
 #include $(PREBUILT_STATIC_LIBRARY)
 
-#bullet
-include $(CLEAR_VARS)
-LOCAL_MODULE := bullet
-LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libBulletSoftBody.a
-LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libBulletDynamics.a
-LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libBulletCollision.a
-LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libLinearMath.a
-LOCAL_EXPORT_C_INCLUDES := $(ANDROID_TOOLCHAIN)/include/bullet
-include $(PREBUILT_STATIC_LIBRARY)
+##bullet
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := bullet
+#LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libBulletSoftBody.a
+#LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libBulletDynamics.a
+#LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libBulletCollision.a
+#LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libLinearMath.a
+#LOCAL_EXPORT_C_INCLUDES := $(ANDROID_TOOLCHAIN)/include/bullet
+#include $(PREBUILT_STATIC_LIBRARY)
 
 #assimp
 include $(CLEAR_VARS)
@@ -45,6 +45,6 @@ LOCAL_CFLAGS    += -march=armv7-a -mfloat-abi=softfp -mfpu=vfp -ffunction-sectio
 LOCAL_CPPFLAGS  += -std=c++11 -O2
 LOCAL_CPPFLAGS  += -march=armv7-a -mfloat-abi=softfp -mfpu=vfp -ffunction-sections -funwind-tables -fstack-protector -fno-short-enums -DANDROID -Wa,--noexecstack -std=gnu++0x -mtune=cortex-a9 -march=armv7-a -mhard-float -mfloat-abi=softfp -mfpu=vfpv3-d16 -g -g -gdwarf-2 -marm -O0 -fno-omit-frame-pointer -Wall -W -D_REENTRANT -fPIE -DQT_QML_DEBUG -DQT_DECLARATIVE_DEBUG -DQT_OPENGL_LIB -DQT_WIDGETS_LIB -DQT_GUI_LIB -DQT_CORE_LIB 
 LOCAL_SRC_FILES := ../../common/game.cpp jni.cpp assets_file_io.cpp
-LOCAL_STATIC_LIBRARIES := assimp bullet libpng
+LOCAL_STATIC_LIBRARIES := assimp libpng #bullet
 LOCAL_LDLIBS 	:= -lz -lstdc++ -llog -lGLESv2 -lEGL -landroid
 include $(BUILD_SHARED_LIBRARY)
