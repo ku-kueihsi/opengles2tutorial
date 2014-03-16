a19e0cd33177fb73ca4e2b8566b0c233d801102b
Jiaxi Hu <jiaxihu@gmail.com>
2014-03-15 23:15:43 -0400

    roll back to triangle

--- src/platform/android/jni/Android.mk
+++ src/platform/android/jni/Android.mk
@@ -1,14 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
-#bullet
-include $(CLEAR_VARS)
-LOCAL_MODULE := bullet
-LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libBulletSoftBody.a
-LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libBulletDynamics.a
-LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libBulletCollision.a
-LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libLinearMath.a
-LOCAL_EXPORT_C_INCLUDES := $(ANDROID_TOOLCHAIN)/include/bullet
-include $(PREBUILT_STATIC_LIBRARY)
+##bullet
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := bullet
+#LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libBulletSoftBody.a
+#LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libBulletDynamics.a
+#LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libBulletCollision.a
+#LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libLinearMath.a
+#LOCAL_EXPORT_C_INCLUDES := $(ANDROID_TOOLCHAIN)/include/bullet
+#include $(PREBUILT_STATIC_LIBRARY)
 
 #assimp
 include $(CLEAR_VARS)
@@ -24,12 +24,12 @@ LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libpng.a
 LOCAL_EXPORT_C_INCLUDES := $(ANDROID_TOOLCHAIN)/include/
 include $(PREBUILT_STATIC_LIBRARY)
 
-#libz
-include $(CLEAR_VARS)
-LOCAL_MODULE := libz
-LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libz.a
-LOCAL_EXPORT_C_INCLUDES := $(ANDROID_TOOLCHAIN)/include/
-include $(PREBUILT_STATIC_LIBRARY)
+##libz
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libz
+#LOCAL_SRC_FILES := $(ANDROID_TOOLCHAIN)/lib/libz.a
+#LOCAL_EXPORT_C_INCLUDES := $(ANDROID_TOOLCHAIN)/include/
+#include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
@@ -43,8 +43,8 @@ LOCAL_MODULE    := game
 LOCAL_CFLAGS    := -Wall -Wextra
 LOCAL_CPPFLAGS  += -std=c++11
 LOCAL_SRC_FILES := ../../common/game.cpp jni.cpp assets_file_io.cpp
-LOCAL_STATIC_LIBRARIES := assimp bullet libpng libz
-LOCAL_LDLIBS 	:= -lstdc++ -llog -lGLESv2 -lEGL -landroid
+LOCAL_STATIC_LIBRARIES := assimp libpng
+LOCAL_LDLIBS 	:= -lstdc++ -lz -llog -lGLESv2 -lEGL -landroid
 
 
 include $(BUILD_SHARED_LIBRARY)
